e28070b4cf844bfb4a8d70da3f54f1eb7fb16199
もうせ <js.mouse726@gmail.com>
2015-09-16 17:41:36 +0900

    画像名変更したよ、画像表示するもの増やしたよ

--- UI/javascripts/pngExpoter.js
+++ UI/javascripts/pngExpoter.js
@@ -1,33 +1,99 @@
-var board_num = 0;
 
-var left = ['man.png',　'woman.png'
-            ,'youjo.png',　'hamburger.png'
-            ,'youjo.png'];
-var center = ['man.png',　'woman.png'
-            ,'youjo.png',　'hamburger.png'
-            ,'youjo.png'];
-var right = ['man.png',　'woman.png'
-            ,'youjo.png',　'hamburger.png'
-            ,'youjo.png'];
+var left = {ame: 'ame.png', car: 'car.png', cat: 'cat.png'
+            , hamburger: 'hamburger.png', karaoke: 'karaoke.png'
+            , kisskidf: 'kisskidf.png', kisskidm: 'kisskidm.png'
+            , man: 'man.png', motituki: 'motituki.png'
+            , none: 'none.png', nozoki: 'nozoki.png'
+            , panda: 'panda.png', run: 'run.png'
+            , samurai: 'samurai.png', syagamu: 'syagamu.png'
+            , tomb: 'tomb.png', woman: 'woman.png'
+            , youjo: 'youjo.png'}
+
+var center = {banzai: 'banzai.png', barerina: 'barerina.png', chair: 'chair.png'
+            , heart: 'heart.png', lead: 'lead.png'
+            , nessi: 'nessi.png', none: 'none.png'
+            , ochai: 'ochai.png', skul: 'skul.png'
+            , unko: 'unko.png', ushi: 'ushi.png'}
+
+var center = {car: 'car.png', cat: 'cat.png'
+            , hamburger: 'hamburger.png', karaoke: 'karaoke.png'
+            , kisskidf: 'kisskidf.png', kisskidm: 'kisskidm.png'
+            , man: 'man.png', motituki: 'motituki.png'
+            , none: 'none.png', nozoki: 'nozoki.png'
+            , panda: 'panda.png', run: 'run.png'
+            , samurai: 'samurai.png', syagamu: 'syagamu.png'
+            , tomb: 'tomb.png', woman: 'woman.png'
+            , youjo: 'youjo.png', numarumi: 'nukarumi.png'}
 
 var board = ['./images/board/blue.png','./images/board/yellow.png'
-                ,'./images/board/red.png'];
+             ,'./images/board/red.png'];
 
-var numMaterials = fileArry.length;
-var loadedCounter = 0;
 var imgObjArry = [];
 var canvas = document.getElementById('canvasElem');
 var ctx = canvas.getContext('2d');
-var type = 'image/png';
+//var type = 'image/png';
+var board_num = 1;
 
-function stamp(num) {
+if (board_num == 1) {
     var img = new Image();
-
     img.onload = (function() {
-        ctx.drawImage(img, 0, 0, 400, 400);
+    ctx.drawImage(img, 0, 0, 400, 400);
     });
+    img.src = board[0];
+} else if (board_num == 2) {
+    var img = new Image();
+    img.onload = (function() {
+    ctx.drawImage(img, 0, 0, 400, 400);
+    });
+    img.src = board[1];
+} else if (board_num == 3) {
+    var img = new Image();
+    img.onload = (function() {
+    ctx.drawImage(img, 0, 0, 400, 400);
+    });
+    img.src = board[2];
+}
 
-    img.src = fileArry[num];
+
+function stamp(type, name) {
+    if (type == 'left') {
+        var img = new Image();
+        img.onload = (function() {
+            ctx.drawImage(img, 0, 0, 400, 400);
+        });
+        if (board_num == 1) {
+            img.src = "./images/left/1_" + left[name];
+        } else if (board_num == 2) {
+            img.src = "./images/left/2_" + left[name];
+        } else if (board_num == 3) {
+            img.src = "./images/left/3_" + left[name];
+        }
+    } else if (type == 'center') {
+        var img = new Image();
+        img.onload = (function() {
+            ctx.drawImage(img, 0, 0, 400, 400);
+        });
+        if (board_num == 2) {
+            img.src = "./images/center/1_" + center[name];
+        } else if (board_num == 2) {
+            img.src = "./images/center/2_" + center[name];
+        } else if (board_num == 3) {
+            img.src = "./images/center/3_" + center[name];
+        }
+        img.src = left[type];
+    } else if (type == 'right') {
+        var img = new Image();
+        img.onload = (function() {
+            ctx.drawImage(img, 0, 0, 400, 400);
+        });
+        if (board_num == 3) {
+            img.src = "./images/right/1_" + right[name];
+        } else if (board_num == 2) {
+            img.src = "./images/right/2_" + right[name];
+        } else if (board_num == 3) {
+            img.src = "./images/right/3_" + right[name];
+        }
+    }
 }
 
 
